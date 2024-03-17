fbaa292af306213cf03b388b3826fe49bf27fc21
alex11920 <av61799@gmail.com>
2024-03-17 09:17:46 +0600

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,8 +8,8 @@ const Hero = () => {
                 <div className="hero-overlay absolute inset-0 bg-gradient-to-r from-[#150B2BB2] bg-opacity-60 rounded-3xl"></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-[800px] mx-auto py-10 lg:py-0 relative">
-                        <h1 className="mb-6 text-5xl font-bold">Discover an exceptional cooking class tailored for you!</h1>
-                        <p className="mb-10">Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database and solve 500+ coding problems to become an exceptionally well world-class Programmer.</p>
+                        <h1 className="mb-6 text-5xl font-bold">Embark on a Culinary Adventure with Personalized Cooking Classes!</h1>
+                        <p className="mb-10">Indulge in personalized cooking classes! Explore diverse cuisines, uncover culinary secrets, and create masterpieces. Join us for an unforgettable culinary journey!</p>
                         <div className="flex gap-4 justify-center">
                             <button className="btn back-original text-white border-none rounded-full">Explore Now</button>
                             <button className="btn bg-transparent text-white border rounded-full">Our Feedback</button>
@@ -20,13 +20,9 @@ const Hero = () => {
 
             <div className="text-center mt-16 lg:mt-24 px-6">
                 <h1 className="text-4xl font-semibold">Our Recipes</h1>
-                <p className="second mt-3 mb-12 max-w-[850px] mx-auto">Lorem ipsum dolor sit amet consectetur. Proin et feugiat senectus vulputate netus pharetra rhoncus. Eget urna volutpat curabitur elementum mauris aenean neque.</p>
+                <p className="second mt-3 mb-12 max-w-[850px] mx-auto">Embark on a culinary journey with our curated selection of delectable recipes.</p>
             </div>
         </div>
-
-
-
-
     );
 };
 
